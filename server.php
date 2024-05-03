759306565bacfc7d1dda093c8199015683d8a68d
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-03 15:41:24 +0200

    ADD: censured text lenght

--- server.php
+++ server.php
@@ -9,8 +9,11 @@ $array_text = explode(' ', $text); //words
 
 $array_length = count($array_text); //number of words
 
+$censured_message = str_replace($bad_word, '***', $text); //message with ***
+$censured_message_words = array_diff($array_text, [$bad_word]); //censured words
 
-$censured_message = str_replace($bad_word, '***', $text);
+// var_dump($censured_message_words);
+$censured_array_length = count($censured_message_words);
 ?>
 
 
@@ -25,20 +28,26 @@ $censured_message = str_replace($bad_word, '***', $text);
             color: red;
             font-size: 40px;
         }
+        .text{
+            color: blue;
+        }
     </style>
 </head>
 <body>
     <main>
         <h1>La parola da censurare è: </h1>
         <h2><?php echo $bad_word; ?></h2>
-        <p>
+        <p  class="text">
         <?php echo $text; ?>
         </p>
         <p>
             Il tuo testo è lungo <strong><?php echo $array_length; ?></strong> parole.
         </p>
-        <p>
+        <p  class="text">
         <?php echo $censured_message; ?>
+        </p>
+        <p>
+            Il tuo testo senza parole censurate è lungo <strong><?php echo $censured_array_length; ?></strong> parole.
         </p>
         
     </main>
